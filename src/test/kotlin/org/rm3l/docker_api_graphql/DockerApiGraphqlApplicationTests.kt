9944fd1bc2f75c4c09bd16e639edd9fd66c05fa9
Armel Soro <armel@rm3l.org>
2020-12-06 21:39:32 +0100

    Fix test classes compilation issues
    Use Junit5 annotations

--- src/test/kotlin/org/rm3l/docker_api_graphql/DockerApiGraphqlApplicationTests.kt
+++ src/test/kotlin/org/rm3l/docker_api_graphql/DockerApiGraphqlApplicationTests.kt
@@ -23,12 +23,9 @@
 
 package org.rm3l.docker_api_graphql
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 class DockerApiGraphqlApplicationTests {
 
